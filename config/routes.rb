49c96f0d4f0a1b0bc30d289ccb48ef8e09c5db78
Brady Berryessa <you@example.com>
2018-01-05 23:39:09 -0700

    changed the routes so they were more clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+  resources :blogs
 
   root to: 'abode#homepage'
-
-  get 'blog/index', to: 'blog#index'
-  get 'blog/create', to: 'blog#create'
-  get 'blog/new', to: 'blog#new'
-  get 'blog/destroy', to: 'blog#desroy'
-  get 'blog/show', to: 'blog#show'
 
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -4,17 +4,17 @@
 
 #   describe 'index' do 
 #     it 'can be reached successfully' do
-#       visit blog_index_path
+#       visit blogs_path
 #       expect(page.status_code).to eq(200)
 #     end
 
 #     it "has a title of 'Justin's team won and the rest of us lost" do
-#       visit blog_index_path
+#       visit blogs_path
 #       expect(page).to have_content(/Justin's team won and the rest of us lost/)
 #     end
 
 #     it 'can be reached successfully' do
-#       visit blog_new_path
+#       visit new_blog_path
 #       expect(page.status_code).to eq(200)
 #     end
 #   end
