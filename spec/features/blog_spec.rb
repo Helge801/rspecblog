a01c0cb4264c2b36c2c4ae83fe3e658a32c3a107
Brady Berryessa <you@example.com>
2018-01-05 23:30:13 -0700

    Commented out the tests so that we can test them one by one.

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,33 +1,33 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'navigate' do
+# describe 'navigate' do
 
-  describe 'index' do 
-    it 'can be reached successfully' do
-      visit blog_index_path
-      expect(page.status_code).to eq(200)
-    end
+#   describe 'index' do 
+#     it 'can be reached successfully' do
+#       visit blog_index_path
+#       expect(page.status_code).to eq(200)
+#     end
 
-    it "has a title of 'Justin's team won and the rest of us lost" do
-      visit blog_index_path
-      expect(page).to have_content(/Justin's team won and the rest of us lost/)
-    end
+#     it "has a title of 'Justin's team won and the rest of us lost" do
+#       visit blog_index_path
+#       expect(page).to have_content(/Justin's team won and the rest of us lost/)
+#     end
 
-    it 'can be reached successfully' do
-      visit blog_new_path
-      expect(page.status_code).to eq(200)
-    end
-  end
+#     it 'can be reached successfully' do
+#       visit blog_new_path
+#       expect(page.status_code).to eq(200)
+#     end
+#   end
 
-  describe 'Creation' do
-    it 'has a new form that can be reached' do
-      fill_in 'blog[title]', with 'Some title'
-      fill_in 'blog[body]', with 'Body'
-      fill_in 'blog[image]', with 'NyanCat'
+#   describe 'Creation' do
+#     it 'has a new form that can be reached' do
+#       fill_in 'blog[title]', with: 'Some title'
+#       fill_in 'blog[body]', with: 'Body'
+#       fill_in 'blog[image]', with: 'NyanCat'
 
-      click_on "Save"
+#       click_on "Save"
 
-      expect(page).to have_content("Body")
-    end
-  end
-end
+#       expect(page).to have_content("Body")
+#     end
+#   end
+# end
