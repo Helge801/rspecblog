628d0fc3a00ce3e29f8793f1b11efa7f1ee011f2
Brady Berryessa <you@example.com>
2018-01-05 19:40:07 -0700

    Made test to make sure there is content on index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-root to: 'abode#homepage'
+  root to: 'abode#homepage'
 
-get 'index', to: 'blogs#index'
+  get 'blog/index', to: 'blog#index'
+  get 'blog/create', to: 'blog#create'
+  get 'blog/new', to: 'blog#new'
+  get 'blog/destroy', to: 'blog#desroy'
+  get 'blog/show', to: 'blog#show'
 
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  
   describe 'index' do 
     it 'can be reached successfully' do
-      visit index_path
+      visit blog_index_path
       expect(page.status_code).to eq(200)
+    end
+
+    it "has a title of 'Justin's team won and the rest of us lost" do
+      visit blog_index_path
+      expect(page).to have_content(/Justin's team won and the rest of us lost/)
     end
   end
 end
