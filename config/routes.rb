ec73456ee1290eee408732f6eebf5e5df47a542a
Brady Berryessa <you@example.com>
2018-01-05 19:02:13 -0700

    Created test for blogs that validates presence of title, body, and image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :blogs
 root to: 'abode#homepage'
 
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do
+    before do 
+      @blog = Blog.create(title: "Anything", body: "Anything", image: "Anything")
+    end
+    it 'can be created' do
+      expect(@blog).to be_valid
+    end
+
+    it 'cannot be created without a title, body, or image' do
+      @blog.title = nil
+      @blog.body = nil
+      @blog.image = nil
+      expect(@blog).to_not be_valid
+    end
+  end
 end
