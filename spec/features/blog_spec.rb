952132169954216b7bce2634d8c252f46a78ba9b
Brady Berryessa <you@example.com>
2018-01-05 23:23:54 -0700

    Added a few more tests

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  
+
   describe 'index' do 
     it 'can be reached successfully' do
       visit blog_index_path
@@ -11,6 +11,23 @@ describe 'navigate' do
     it "has a title of 'Justin's team won and the rest of us lost" do
       visit blog_index_path
       expect(page).to have_content(/Justin's team won and the rest of us lost/)
+    end
+
+    it 'can be reached successfully' do
+      visit blog_new_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'Creation' do
+    it 'has a new form that can be reached' do
+      fill_in 'blog[title]', with 'Some title'
+      fill_in 'blog[body]', with 'Body'
+      fill_in 'blog[image]', with 'NyanCat'
+
+      click_on "Save"
+
+      expect(page).to have_content("Body")
     end
   end
 end
